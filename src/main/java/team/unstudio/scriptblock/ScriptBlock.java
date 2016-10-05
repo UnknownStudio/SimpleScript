a3bcc0e09db4a544d7abeb05dd874e308bfc7d18
ningwang747 <ningwang2002@icloud.com>
2016-10-05 17:06:35 +0800

    Add space

--- src/main/java/team/unstudio/scriptblock/ScriptBlock.java
+++ src/main/java/team/unstudio/scriptblock/ScriptBlock.java
@@ -5,16 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class ScriptBlock extends JavaPlugin {
 	@Override
 	public final void onLoad() {
-
+		
 	}
 
 	@Override
 	public final void onEnable() {
-
+		
 	}
 
 	@Override
 	public final void onDisable() {
-
+		
 	}
 }
