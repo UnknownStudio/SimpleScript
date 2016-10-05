2e4e3a150b8fa208be7e90f9e5e9d912c3688c22
ningwang747 <ningwang2002@icloud.com>
2016-10-05 16:57:20 +0800

    Initial commit again

---
+++ src/main/java/team/unstudio/scriptblock/ScriptBlock.java
@@ -0,0 +1,20 @@
+package team.unstudio.scriptblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScriptBlock extends JavaPlugin {
+	@Override
+	public final void onLoad() {
+
+	}
+
+	@Override
+	public final void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
