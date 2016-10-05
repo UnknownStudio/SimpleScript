3bd9d5f033e5c542b03ae20a14d6dd76c8324adf
ningwang747 <ningwang2002@icloud.com>
2016-10-05 23:09:45 +0800

    Fix

--- src/main/java/team/unstudio/simplescript/support/Vault.java
+++ src/main/java/team/unstudio/simplescript/support/Vault.java
@@ -1,17 +1,21 @@
 package team.unstudio.simplescript.support;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
+import net.milkbowl.vault.economy.Economy;
+
 public class Vault {
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
+	@SuppressWarnings("unused")
+	private boolean setupEconomy() {
+		if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		Economy economy = rsp.getProvider();
+		return economy != null;
+	}
 }
