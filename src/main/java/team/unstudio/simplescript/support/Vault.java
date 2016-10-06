b383ec3314d20ecfc62063c91b5d8f9b394812a4
ningwang747 <ningwang2002@icloud.com>
2016-10-06 19:23:40 +0800

    Performance optimization

--- src/main/java/team/unstudio/simplescript/support/Vault.java
+++ src/main/java/team/unstudio/simplescript/support/Vault.java
@@ -1,26 +1,28 @@
 package team.unstudio.simplescript.support;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 import net.milkbowl.vault.economy.Economy;
 
 public class Vault {
 	public static final Economy economy = setupEconomy();
-	
+
 	private static Economy setupEconomy() {
-		//Compressed from
+		// Compressed from
 		/*
-		if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
-			return null;
-		}
-		RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
-		if (rsp == null) {
-			return null;
-		}
-		return rsp.getProvider();
+		* if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
+		*		return null;
+		* }
+		* RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+		* if (rsp == null) {
+		*		return null;
+		* }
+		* return rsp.getProvider();
 		*/
-		return Bukkit.getServer().getPluginManager().getPlugin("Vault") == null
-				&& Bukkit.getServer().getServicesManager().getRegistration(Economy.class) == null ? null
-						: Bukkit.getServer().getServicesManager().getRegistration(Economy.class).getProvider();
+		Server server = Bukkit.getServer();
+		RegisteredServiceProvider<Economy> rsp = server.getServicesManager().getRegistration(Economy.class);
+		return server.getPluginManager().getPlugin("Vault") == null && rsp == null ? null : rsp.getProvider();
 	}
 }
